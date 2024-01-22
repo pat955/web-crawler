099da3f56760dfa86050d98f7961e00bb79ccd4f
pat955 <patricijap8@gmail.com>
2024-01-22 12:43:12 +0100

    normalizeURL working

---
+++ crawl.js
@@ -0,0 +1,11 @@
+function normalizeURL(url){
+    if (url[url.length-1]==='/'){
+        return url.slice(url.indexOf('/')+2, url.length-1)
+    }
+    return url.slice(url.indexOf('/')+2)
+}
+
+module.exports = {
+    normalizeURL
+  }
+  
