1abc671ad3e9e93980469973f451f4c1f065265f
pat955 <patricijap8@gmail.com>
2024-01-22 14:11:10 +0100

    getting url from html working

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+const { JSDOM } = require('jsdom')
+
 function normalizeURL(url){
     if (url[url.length-1]==='/'){
         return url.slice(url.indexOf('/')+2, url.length-1)
@@ -5,7 +7,14 @@ function normalizeURL(url){
     return url.slice(url.indexOf('/')+2)
 }
 
+function getURLfromHTML(htmlBody, baseURL){
+    const dom  = new JSDOM(htmlBody)
+    const links = dom.window.document.querySelectorAll("a")
+    const hrefAttributes = Array.from(links).map(link => link.href);
+    return hrefAttributes
+}
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLfromHTML
   }
-  
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,8 @@
 const { test, expect } = require('@jest/globals')
 const { normalizeURL } = require('./crawl.js')
+const { getURLfromHTML } = require('./crawl.js')
+
+// Normalize Url 
 
 test('https://blog.boot.dev/path/ to blog.boot.dev/path', () => {
     expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
@@ -17,6 +20,27 @@ test('http://blog.boot.dev/path/v1 to blog.boot.dev/path/v1', () => {
     expect(normalizeURL('http://blog.boot.dev/path/v1')).toBe('blog.boot.dev/path/v1');
   });
 
+// Get urls from html
+test('test 1', ()=> {
+    expect(getURLfromHTML(
+    `<html>
+        <body>
+            <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>
+        </body>
+    </html>`, 'https://blog.boot.dev'
+    )).toStrictEqual(['https://blog.boot.dev/'])
+})
+
+test('test 2', ()=> {
+    expect(getURLfromHTML(
+    `<html>
+        <body>
+            <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>
+            <a href="http://example.com"><span>example</span></a>
+        </body>
+    </html>`, 'https://blog.boot.dev'
+    )).toStrictEqual(['https://blog.boot.dev/', 'http://example.com/'])
+})
 //   https://blog.boot.dev/path/
 //   https://blog.boot.dev/path
 //   http://blog.boot.dev/path/
