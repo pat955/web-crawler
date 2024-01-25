e23bd87c7c5b153df710b6092b1048b6ec2b9642
pat955 <patricijap8@gmail.com>
2024-01-25 13:15:18 +0100

    Semi working

--- crawl.js
+++ crawl.js
@@ -14,29 +14,36 @@ function getURLfromHTML(htmlBody, baseURL){
     const hrefAttributes = Array.from(links).map(link => link.href);
     return hrefAttributes.toString().split(',')
 }
+function updatePages(pages, newPages){
+    print(newPages)
+    for (const [key, value] of Object.entries(newPages)){
+        pages[key] += value
+    }
+}
 
 async function crawlPage(baseURL, currentURL, pages){
     const normURL = normalizeURL(currentURL)
     if (!(normURL.startsWith(normalizeURL(baseURL)))){
+        return pages}
+    
+    if (!(normURL in pages)){
+        pages[normURL] = 1}
+    else{
         return pages
     }
-
-    if (!(normURL in pages)){
-        pages[normURL] = 0
-    }
-    pages[normURL]++
-    const response = await fetch(currentURL)
     
+    
+    print(normURL)
+    const response = await fetch(currentURL)
     const html = await response.text()
     const allURLs = await getURLfromHTML(html)
     
     for (let url of allURLs){
-        print(url)
         if (url[0] === '/' && url.length > 2){
-            url =`${baseURL}${url}`
-        }
+            url =`${baseURL}${url}`}
+        pages = await crawlPage(baseURL, url, pages)
         
-    }
+    }   
     return pages
 }  
 
