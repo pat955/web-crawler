068a17b9c348f6f66886b8f8a2b3080f25321fd3
pat955 <patricijap8@gmail.com>
2024-03-11 11:05:03 +0100

    search function underway

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-const { Console, time } = require('console')
 const { crawlPage } = require('./crawl.js')
-const { printReport } = require('./report.js')
-const { normalizeURL } = require('./crawl.js')
 const fs = require('fs');
 
 
@@ -9,6 +6,7 @@ async function main(){
     const args = process.argv.slice(2);
     if (args.length != 1){
         throw new Error(`One argument needed, you have: ${args.length}` )}
+
     const baseURL = args[0]
     console.log(`${baseURL}\nStarting crawling...`)
     
@@ -17,6 +15,7 @@ async function main(){
 
     if (fs.existsSync('cache.json')){
         const json = JSON.parse(fs.readFileSync("cache.json"))
+
         if (baseURL in json["urls"]){
             console.log(json["urls"][baseURL]["page count"])
             return 
@@ -36,19 +35,33 @@ async function main(){
         const pages = {}
         const pageCount = await crawlPage(baseURL, baseURL, pages)
         console.log(pageCount)
-        
+
         const newCacheFile = JSON.stringify({"urls": {[baseURL] : {"page count" : pageCount, "cache time": today}}}, undefined, 4)
         fs.writeFile('cache.json', newCacheFile, (err) => err && console.error(err))
     }    
 }
 
+function searchCache(keyword, baseURL){
+    const json = JSON.parse(fs.readFileSync("cache.json"))
+    if (!baseURL in json["urls"]){
+        throw new Error(`You have not crawled through this url before` )
+    }
+    const array = []
+    const pages = json["urls"][baseURL]["page count"]
+    for (const [page, count] of Object.entries(pages)){
+        // remove the base url from page so that it only searches the subpages
+        if (page.match(keyword)){
+            array.push(`https://${page}`)
+        }
+    }
+    return array
+}
+
 main()
 
 // Ideas
-// Make the script run on a timer and deploy it to a server. Have it email you every so often with a report.
 // Add more robust error checking so that you can crawl larger sites without issues.
 // Count external links, as well as internal links, and add them to the report
-// Save the report as a CSV spreadsheet rather than printing it to the console
 // Use a graphics library to create an image that shows the links between the pages as a graph visualization
 // Make requests concurrently to speed up the crawling process
 // Add a README.md file explaining to users how to clone your git repo and get started
